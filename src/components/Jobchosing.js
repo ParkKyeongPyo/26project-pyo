5dbec987813615abaebd0a5cd3c4f4459d6e08b9
ParkKyeongPyo <52643354+ParkKyeongPyo@users.noreply.github.com>
2022-08-17 22:00:30 +0900

    수정

--- src/components/Jobchosing.js
+++ src/components/Jobchosing.js
@@ -110,7 +110,7 @@ function Jobchosing({
   return (
     <div style={{ height: "inherit", marginLeft: "auto", marginRight: "auto" }}>
       <animated.div style={animation1} className={common.flexRowh2}>
-        "워크얼론(Work Alone)은 대한민국에서 혼자 일하는 모든 분들을 위한 커뮤니티입니다:)"
+        "혼자번당은 대한민국에서 혼자 일하는 모든 분들을 위한 커뮤니티입니다"
       </animated.div>
 
       <animated.div style={animation2} className={common.flexRowMiddle}>
